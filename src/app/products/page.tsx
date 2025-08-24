d20120f4a11698d2e9460b16498c53c3f78cedf2
Bandeira <bandeiradev.ti@gmail.com>
2025-08-24 10:39:59 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,8 +3,8 @@ import { Input } from "@/components/ui/input";
 
 const ProdutctPage = () => {
     return ( 
-        <div className="flex justify-center items-center h-screen flex-col">
-            <h2 className="text-red-500">products</h2>
+        <div className="flex justify-center items-center h-screen flex-col p-5">
+            <h2 className="p-5 text-red-500">products</h2>
             <Button>FSW 7.0</Button>
             <Input placeholder="Bora fechar esse projeto?" />
         </div>
